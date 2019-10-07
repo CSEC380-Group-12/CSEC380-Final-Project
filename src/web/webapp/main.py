311cd0d4eb0fb4aa1fd558e88d019eb5d63c7459
Scott Court <z5t1@z5t1.com>
2019-10-07 12:21:12 -0400

    Added webapp skeleton

--- src/web/webapp/main.py
+++ src/web/webapp/main.py
@@ -2,7 +2,26 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# Session variables:
+# is_logged_in		boolean: True if the session is logged in, otherwise
+#					False/None
+# uid				int: The UID of the user that is logged in
+
 @app.route('/')
-def index():
-	return "<h1>Hello World!</h1>"
+def route_index():
+	return render_template('login.html')
+
+@app.route('/login', method='POST')
+def route_login():
+	# TODO
+	print("Username: "+request.form['username'])
+	print("Password: "+request.form['password'])
+
+@app.route('/logout', method='POST')
+def route_logout():
+	# TODO
+	print(flask.session)
+
+# vim:tabstop=4
+# vim:shiftwidth=4
 
