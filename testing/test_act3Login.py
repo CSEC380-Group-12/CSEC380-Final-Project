fb9b64954f31101b656ba294778557a5bd661827
Brendy Flannel <48101584+brendan1227@users.noreply.github.com>
2019-11-20 21:52:26 -0500

    Added Time and Sleep
    Added sleep(2) for Travis testing.

--- testing/test_act3Login.py
+++ testing/test_act3Login.py
@@ -8,33 +8,37 @@ Author: Brendan
 from lxml import html
 import requests
 import pytest
-
+import time
 
 def test_login():
     print("Connecting to site...")
     body = html.parse('http://localhost:80').xpath('//body')[0].text_content()
+    time.sleep(2)
     assert "Username" in body.strip()
     print("Connected!\n")
   
     print("Logging In: Username = brendy, Pass = flannle")
     loginInfo = {"username": "brendy", "password": "flannle"}
     req = requests.post('http://localhost:80/login', data=loginInfo)
+    time.sleep(2)
     assert "UPLOAD" in req.text
     print("Logged In!\n")
 
     print("Attempting Wrong Pass: Username = brendy, Pass = wrongPass...")
     data = {"username": "brendy", "password": "wrongPass"}
     req = requests.post('http://localhost:80/login', data=data)
+    time.sleep(2)
     assert "doesn't exist" in req.text
     print("Log in Failed:\n")
     
     print("Attempting Wrong Username: Username = brendy1, Pass = flannle")
     data = {"username": "brendy1", "password": "flannle"}
     req = requests.post('http://localhost:80/login', data=data)
-    assert "doesn't exist" in req.text
+    time.sleep(2)
     print("Log in Failed\n")
     
     print("End of Test")
     assert "doesn't exist" in req.text
 
-
+if __name__ == '__main__':
+    test_login()
