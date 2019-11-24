feeae1bb73edf50e020c5147819c861b0fa1eb92
Scott Court <z5t1@z5t1.com>
2019-11-24 00:56:34 -0500

    Added Classic SQL and Command Injection Videos

--- src/database/db_init/020_add_users.sql
+++ src/database/db_init/020_add_users.sql
@@ -1,11 +1,11 @@
 USE webapp_db;
 
- INSERT INTO accounts(username, pass_hash) VALUES
+INSERT INTO accounts(username, pass_hash) VALUES
 	('flask', '319c3206a7f10c17c3b9116d4a957646');	-- password=flask
 
 INSERT INTO accounts(username, pass_hash) VALUES
 	('root', '7b24afc8bc80e548d66c4e7ff72171c5');	-- password=toor
 
 
- INSERT INTO accounts(username, pass_hash) VALUES
+INSERT INTO accounts(username, pass_hash) VALUES
 	('brendy', '6f699ad2c622dec763affcfa311719ba');	-- password=flannle
