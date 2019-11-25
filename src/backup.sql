910f762a5c57011758ff7bb634dad744115c9375
brend <blm4271@rit.edu>
2019-11-24 23:59:31 -0500

    database

---
+++ src/backup.sql
@@ -0,0 +1,1 @@
+OCI runtime exec failed: exec failed: container_linux.go:345: starting container process caused "exec: \"/user/bin/mysqldump\": stat /user/bin/mysqldump: no such file or directory": unknown
